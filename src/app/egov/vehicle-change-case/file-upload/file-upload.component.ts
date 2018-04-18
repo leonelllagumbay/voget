b7d7221dd4fc5f7d6e0feed019dca69ae0a51bb7
Leonell Lagumbay <lale@erni.ph>
2018-04-18 12:16:26 +0800

    add file upload component files

---
+++ src/app/egov/vehicle-change-case/file-upload/file-upload.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-file-upload',
+  templateUrl: './file-upload.component.html',
+  styleUrls: ['./file-upload.component.scss']
+})
+export class FileUploadComponent implements OnInit {
+
+  form: FormGroup;
+  loading = false;
+
+  @ViewChild('fileInput') fileInput: ElementRef;
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  ngOnInit() {
+
+  }
+
+  createForm() {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      avatar: null
+    });
+  }
+
+  onFileChange(event) {
+    const reader = new FileReader();
+    if (event.target.files && event.target.files.length > 0) {
+      console.log('files', event.target.files);
+      const file = event.target.files[0];
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.form.get('avatar').setValue({
+          filename: file.name,
+          filetype: file.type,
+          value: reader.result.split(',')[1]
+        });
+      };
+    }
+  }
+
+  onSubmit() {
+    const formModel = this.form.value;
+    this.loading = true;
+    // In a real-world app you'd have a http request / service call here like
+    // this.http.post('apiUrl', formModel)
+    setTimeout(() => {
+      console.log(formModel);
+      alert('done!');
+      this.loading = false;
+    }, 1000);
+  }
+
+  clearFile() {
+    this.form.get('avatar').setValue(null);
+    this.fileInput.nativeElement.value = '';
+  }
+
+}
